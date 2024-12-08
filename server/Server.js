4eef29d39d828077df145aba8bfcae3457f670c7
Roland Ibiwoye <contact@rolandayo.com>
2024-12-09 00:18:49 +0100

    backend

--- server/Server.js
+++ server/Server.js
@@ -7,19 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/employee")
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.error("Could not connect to MongoDB:", err));
+mongoose.connect("mongodb://localhost://127.0.0.1:27017/employee")
 
 app.post("/register", async (req, res) => {
-    try {
-        const employee = await EmployeeModel.create(req.body);
-        res.json(employee);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+    EmployeeModel.create(req.body)
+    .then(employee => res.json(employee))
+    .catch(err => res.json(err))
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3001");
+app.listen (3001, () => {
+    console.log("Server is running on port 3000");
 })
